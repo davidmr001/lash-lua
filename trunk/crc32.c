e3f8c704f20cd228b2640c61560c3d632e4bf021
nrich@ii.net <nrich@ii.net@ce4c58e8-bdc0-11dd-ad1f-f1f9622dbc98>
2008-11-29 03:28:46 +0000

    Cleanup of code.

--- trunk/crc32.c
+++ trunk/crc32.c
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2008 Neil Richardson (nrich@iinet.net.au)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy 
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights 
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+ * copies of the Software, and to permit persons to whom the Software is 
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+ * IN THE SOFTWARE.
+ */
+
 #include <lua.h>
 #include <lauxlib.h>
 #include <lualib.h>
@@ -73,6 +95,12 @@ static unsigned int CRC32[] = {
     3094707162,3040238851,2985771188,
 };
 
+/*
+ * num = lash.CRC32.string2num(str)
+ *
+ *  calculates the CRC32 hash of str
+ *  and returns it as an integer 
+ */
 int CRC32Int(lua_State *L) {
     size_t length;
     unsigned char *data = (unsigned char *)luaL_checklstring(L, 1, &length);
@@ -87,6 +115,12 @@ int CRC32Int(lua_State *L) {
     return 1;
 }
 
+/*
+ * hexstr = lash.CRC32.string2hex(str)
+ *
+ *  calculates the CRC32 hash of str
+ *  and returns it as a lowecase hex string
+ */
 int CRC32String(lua_State *L) {
     size_t length;
     unsigned char *data = (unsigned char *)luaL_checklstring(L, 1, &length);
